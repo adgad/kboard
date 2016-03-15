f87f131ce6051751cef6f85c1307e4b13ea89d6b
Arjun Gadhia <arjungadhia@gmail.com>
2016-03-15 19:51:26 +0000

    Add backup and restore

---
+++ app/src/main/java/com/adgad/kboard/MyBackupAgent.java
@@ -0,0 +1,21 @@
+package com.adgad.kboard;
+
+
+import android.app.backup.BackupAgentHelper;
+import android.app.backup.SharedPreferencesBackupHelper;
+
+public class MyBackupAgent extends BackupAgentHelper {
+
+    // The name of the SharedPreferences file
+    static final String PREFS = "com.adgad.kboard_preferences";
+
+    // A key to uniquely identify the set of backup data
+    static final String PREFS_BACKUP_KEY = "mybackup";
+
+    @Override
+    public void onCreate() {
+        SharedPreferencesBackupHelper helper = new SharedPreferencesBackupHelper(this, PREFS);
+        addHelper(PREFS_BACKUP_KEY, helper);
+    }
+
+}
