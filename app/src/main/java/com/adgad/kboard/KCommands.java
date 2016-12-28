346f6c8ed1758b8a9450a95e1e848db60e77c476
Arjun Gadhia <arjungadhia@gmail.com>
2016-12-28 00:31:34 +0000

    Add start/end of lines

--- app/src/main/java/com/adgad/kboard/KCommands.java
+++ app/src/main/java/com/adgad/kboard/KCommands.java
@@ -2,6 +2,7 @@ package com.adgad.kboard;
 
 import android.view.KeyEvent;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.ExtractedText;
 import android.view.inputmethod.ExtractedTextRequest;
 import android.view.inputmethod.InputConnection;
 
@@ -13,7 +14,11 @@ import java.util.Queue;
 /**
  * Created by arjun on 27/12/16.
  */
+
+
 public class KCommands {
+
+    public static final int MAX_LOOKBACK = 500;
 
     InputConnection inputConnection;
     EditorInfo inputEditor;
@@ -23,6 +28,17 @@ public class KCommands {
         inputConnection = ic;
         inputEditor = ei;
     }
+
+
+    private int getCursorPosition() {
+        ExtractedText extracted = inputConnection.getExtractedText(
+                new ExtractedTextRequest(), 0);
+        if (extracted == null) {
+            return -1;
+        }
+        return extracted.startOffset + extracted.selectionStart;
+    }
+
 
     public void d(int n) {
         buffer.push(inputConnection.getTextBeforeCursor(n, 0).toString());
@@ -62,7 +78,7 @@ public class KCommands {
     }
 
     public void yy(int n) {
-        int currentPosition = inputConnection.getExtractedText(new ExtractedTextRequest(), 0).selectionEnd;
+        int currentPosition = getCursorPosition();
         inputConnection.performContextMenuAction(android.R.id.selectAll);
         buffer.push(inputConnection.getSelectedText(0).toString());
         inputConnection.performContextMenuAction(android.R.id.copy);
@@ -128,7 +144,7 @@ public class KCommands {
     }
 
     public void j(int n) {
-        int position = inputConnection.getExtractedText(new ExtractedTextRequest(), 0).selectionEnd - n;
+        int position = getCursorPosition() - n;
         if(position < 0) {
             position = 0;
         }
@@ -136,7 +152,7 @@ public class KCommands {
     }
 
     public void k(int n) {
-        int position = inputConnection.getExtractedText(new ExtractedTextRequest(), 0).selectionEnd + n;
+        int position = getCursorPosition() + n;
         CharSequence textAfterCursor = inputConnection.getTextAfterCursor(n, 0);
         if(textAfterCursor.length() == n) {
             inputConnection.setSelection(position, position);
@@ -148,14 +164,13 @@ public class KCommands {
     //move back a word
     public void b(int n) {
         for(int i=0;i<n;i++) {
-            final int charactersToGet = 30;
             final String splitRegexp = " ";
 
 
             // delete last word letters
-            String[] words = inputConnection.getTextBeforeCursor(charactersToGet, 0).toString().split(splitRegexp);
+            String[] words = inputConnection.getTextBeforeCursor(MAX_LOOKBACK, 0).toString().split(splitRegexp);
             String lastWord = words[words.length - 1];
-            int position = inputConnection.getExtractedText(new ExtractedTextRequest(), 0).selectionEnd - lastWord.length() - 1;
+            int position = getCursorPosition() - lastWord.length() - 1;
             if(position < 0) {
                 position = 0;
             }
@@ -167,25 +182,56 @@ public class KCommands {
     //move forward a word
     public void w(int n) {
         for(int i=0;i<n;i++) {
-            final int charactersToGet = 30;
             final String splitRegexp = " ";
 
-
-            String[] words = inputConnection.getTextAfterCursor(charactersToGet, 0).toString().split(splitRegexp);
+            String[] words = inputConnection.getTextAfterCursor(MAX_LOOKBACK, 0).toString().split(splitRegexp);
             String nextWord = words[0];
 
-            int position = inputConnection.getExtractedText(new ExtractedTextRequest(), 0).selectionEnd + nextWord.length() + 1;
+            int position = getCursorPosition() + nextWord.length() + 1;
             if(inputConnection.getTextAfterCursor(nextWord.length() + 1, 0).length() == nextWord.length() + 1) {
                 inputConnection.setSelection(position, position);
             } else {
                 inputConnection.setSelection(position-1, position-1);
             }
         }
+    }
 
+    //move forward a word
+    public void startOfLine(int n) {
+        for(int i=0;i<n;i++) {
+            final String splitRegexp = "(?<=\\n)";
+
+            String[] lines = inputConnection.getTextBeforeCursor(MAX_LOOKBACK, 0).toString().split(splitRegexp);
+            String lastLine = lines[lines.length - 1];
+            int position = getCursorPosition() - lastLine.length();
+            if(position < 0) {
+                position = 0;
+            }
+            inputConnection.setSelection(position, position);
+        }
+    }
+
+    //move forward a word
+    public void endOfLine(int n) {
+        for(int i=0;i<n;i++) {
+            final String splitRegexp = "\\n";
+
+            String[] lines = inputConnection.getTextAfterCursor(MAX_LOOKBACK, 0).toString().split(splitRegexp);
+            int nextLineLength = lines[0].equals("") ? lines[1].length() + 1 : lines[0].length();
+            int position = getCursorPosition() + nextLineLength;
+            inputConnection.setSelection(position, position);
+        }
     }
 
     public boolean execute(String cmd, int n, String parameter) {
         inputConnection.beginBatchEdit();
+
+        if(cmd.equals("^")) {
+            cmd = "startOfLine";
+        }
+        if(cmd.equals("$")) {
+            cmd = "endOfLine";
+        }
         try {
             if(parameter != null) {
                 getClass().getMethod(cmd, int.class, String.class).invoke(this, n, parameter);
