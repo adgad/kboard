f1deb49d9f594015fed6bb1e88783be3c39d72b8
Arjun Gadhia <arjungadhia@gmail.com>
2021-12-28 19:08:11 +0000

    Import/export keys as JSON

--- app/src/main/java/com/adgad/kboard/PrefsActivity.java
+++ app/src/main/java/com/adgad/kboard/PrefsActivity.java
@@ -15,13 +15,27 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
 /**
  * Created by arjun on 14/03/15.
  */
+
+
 public class PrefsActivity extends PreferenceActivity {
     /**
      * Adds intent extras so fragment opens
      */
+
 
     @Override
     protected boolean isValidFragment (String fragmentName) {
@@ -72,16 +86,129 @@ public class PrefsActivity extends PreferenceActivity {
     public static class SettingsFragment extends PreferenceFragment
         implements SharedPreferences.OnSharedPreferenceChangeListener {
 
+        private final Gson gson = new Gson();
+        SharedPreferences prefs = null;
+        private final int EXPORT_REQUEST_CODE = 1;
+        private final int IMPORT_REQUEST_CODE = 2;
+
+
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-
-
+            prefs = PreferenceManager.getDefaultSharedPreferences(this.getActivity());
             // Load the preferences from an XML resource
             addPreferencesFromResource(R.xml.prefs);
             initSummary(getPreferenceScreen());
 
+            Preference importKeys = (Preference) findPreference("importKeys");
+
+            importKeys.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                    intent.addCategory(Intent.CATEGORY_OPENABLE);
+                    intent.setType("application/json");
+                    startActivityForResult(intent, IMPORT_REQUEST_CODE);
+                    return true;
+                }
+            });
+
+            Preference exportKeys = (Preference) findPreference("exportKeys");
+
+            exportKeys.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+                    intent.addCategory(Intent.CATEGORY_OPENABLE);
+                    intent.setType("application/json");
+                    intent.putExtra(Intent.EXTRA_TITLE, "kboard-keys.json");
+                    startActivityForResult(intent, EXPORT_REQUEST_CODE);
+                    return true;
+                }
+
+            });
+
+
+
+
+        }
+
+        public String isToString(InputStream inputStream) {
+            final int bufferSize = 1024;
+            final char[] buffer = new char[bufferSize];
+            final StringBuilder out = new StringBuilder();
+            Reader in = null;
+            try {
+                in = new InputStreamReader(inputStream, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            for (; ; ) {
+                int rsz = 0;
+                try {
+                    rsz = in.read(buffer, 0, buffer.length);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (rsz < 0)
+                    break;
+                out.append(buffer, 0, rsz);
+            }
+            return out.toString();
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            OutputStream outputStream = null;
+            InputStream inputStream = null;
+            if (requestCode == EXPORT_REQUEST_CODE) {
+
+                String currentVals = prefs.getString(KboardIME.Keys.STORAGE_KEY, "");
+
+                // Note: you may use try-with resources if your API is 19+
+                try {
+                    // InputStream constructor takes File, String (path), or FileDescriptor
+                    // data.getData() holds the URI of the path selected by the picker
+                    Uri uri = data.getData();
+                    outputStream = this.getActivity().getContentResolver().openOutputStream(uri);
+                    outputStream.write(currentVals.getBytes());
+                    outputStream.close();
+                    Toast.makeText(this.getActivity(), "Exported keys to " + uri.getPath(), Toast.LENGTH_LONG).show();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        outputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else if (requestCode == IMPORT_REQUEST_CODE) {
+                try {
+                    inputStream =  this.getActivity().getContentResolver().openInputStream(data.getData());
+                    String keys = isToString(inputStream);
+                    ArrayList<String> keysAsJson = gson.fromJson(keys, ArrayList.class);
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putString(KboardIME.Keys.STORAGE_KEY, keys);
+                    editor.apply();
+                    Toast toast = Toast.makeText(this.getActivity(), "Imported " + keysAsJson.size() + " keys!", Toast.LENGTH_SHORT);
+                    toast.show();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
 
         @Override
